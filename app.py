dc455cddcdf4cadf8a00096819405db315a9eb7d
rachidFMCG <Rachidbaiou315@gmail.com>
2024-05-16 20:19:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,69 @@
 import cv2
 import numpy as np
-from PIL import Image
-import streamlit as st
+import torch
 from ultralytics import YOLO
+import io
+from PIL import Image
+import os
+import streamlit as st
 
 # Charger le modèle YOLO
-model = YOLO('yolov5s.pt')
+model = YOLO('yolov9c.pt')
+# Chemin du dossier temporaire pour stocker les images téléchargées
+UPLOAD_FOLDER = 'uploads'
 
-# Fonction pour traiter l'image
-def process_image(img_np):
-    # Convertir l'image de BGR à RGB
-    img_rgb = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-
-    # Détecter les objets dans l'image
-    results = model(img_np)
-    class_names = model.names
-
-    # Initialiser le compteur d'objets
-    total_objects = 0
-
-    # Dessiner les résultats sur l'image
-    for result in results.xyxy:
-        for *box, conf, cls in result:
-            x1, y1, x2, y2 = map(int, box)  # Convertir les coordonnées en entiers
-            label = f"{class_names[int(cls)]}: {conf:.2f}"  # Créer le label à afficher
-
-            # Dessiner le rectangle
-            cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-            # Dessiner le label
-            cv2.putText(img_rgb, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-            total_objects += 1
-
-    # Ajouter le nombre total d'objets détectés sur l'image
-    total_objects_label = f"Total Objects Detected: {total_objects}"
-    cv2.putText(img_rgb, total_objects_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    # Afficher l'image annotée
-    st.image(img_rgb, channels="RGB")
+# Vérifiez si le dossier existe, sinon, créez-le
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 st.title('YOLO Object Detection')
 
-# Afficher les options pour télécharger une image ou prendre une photo avec la caméra
-option = st.radio("Choisissez une option:", ('Télécharger une image', 'Prendre une photo avec la caméra'))
+# Option pour télécharger une image
+uploaded_file = st.file_uploader("Télécharger une image", type=["jpg", "jpeg", "png"])
 
-if option == 'Télécharger une image':
-    # Afficher le formulaire de téléchargement de fichier
-    uploaded_file = st.file_uploader("Télécharger une image", type=["jpg", "jpeg", "png"])
+# Option pour capturer une image depuis la caméra
+captured_image = st.camera_input(label="Capturer une image", capture_width=640)
 
-    if uploaded_file is not None:
-        # Charger l'image depuis le fichier uploadé
-        img = Image.open(uploaded_file)
-        img_np = np.array(img)
-        process_image(img_np)
+if captured_image is not None:
+    # Convertir l'image capturée en format PIL pour le traitement
+    img = Image.fromarray(captured_image)
+elif uploaded_file is not None:
+    # Charger l'image depuis le fichier uploadé
+    img = Image.open(uploaded_file)
 
-elif option == 'Prendre une photo avec la caméra':
-    # Bouton pour prendre une photo avec la caméra
-    if st.button('Prendre une photo'):
-        # Utiliser st.camera_input() pour capturer une image depuis la caméra
-        img_data = st.image('Prendre une photo', channels='BGR')
-        img_np = np.array(img_data)
-        process_image(img_np)
+# Si une image est disponible
+if 'img' in locals():
+    # Convertir l'image en tableau numpy
+    img_np = np.array(img)
+    
+    # Détecter les objets dans l'image
+    results = model(img_np)
+    class_names = model.names
+    
+    # Initialiser le compteur d'objets
+    total_objects = 0
+    
+    # Dessiner les résultats sur l'image
+    for result in results:
+        boxes = result.boxes.xyxy.cpu().numpy()  # Les coordonnées des bounding boxes
+        scores = result.boxes.conf.cpu().numpy()  # Les scores de confiance
+        classes = result.boxes.cls.cpu().numpy()  # Les classes prédites
+        
+        total_objects += len(boxes)  # Compter les objets détectés
+        
+        for box, score, cls in zip(boxes, scores, classes):
+            x1, y1, x2, y2 = map(int, box)  # Convertir les coordonnées en entiers
+            label = f"{class_names[int(cls)]}: {score:.2f}"  # Créer le label à afficher
+            
+            # Dessiner le rectangle
+            cv2.rectangle(img_np, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            
+            # Dessiner le label
+            cv2.putText(img_np, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    
+    # Ajouter le nombre total d'objets détectés sur l'image
+    total_objects_label = f"Total Objects Detected: {total_objects}"
+    cv2.putText(img_np, total_objects_label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    
+    # Afficher l'image annotée
+    st.image(img_np, channels="BGR")
